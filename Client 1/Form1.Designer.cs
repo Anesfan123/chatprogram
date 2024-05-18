36747410ba836966d3b9077d3016d5f1f2caf305
Anesfan123 <122611452+Anesfan123@users.noreply.github.com>
2024-05-18 20:20:17 +0200

    Add files via upload

---
+++ Client 1/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Client_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.screen_tbx = new System.Windows.Forms.TextBox();
+            this.IP_tbx = new System.Windows.Forms.TextBox();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Message_tbx = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_setUsername = new System.Windows.Forms.Button();
+            this.profilePictureBox = new System.Windows.Forms.PictureBox();
+            this.btn_uploadPicture = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.profilePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen_tbx
+            // 
+            this.screen_tbx.Location = new System.Drawing.Point(514, 166);
+            this.screen_tbx.Multiline = true;
+            this.screen_tbx.Name = "screen_tbx";
+            this.screen_tbx.ReadOnly = true;
+            this.screen_tbx.Size = new System.Drawing.Size(500, 382);
+            this.screen_tbx.TabIndex = 0;
+            // 
+            // IP_tbx
+            // 
+            this.IP_tbx.Location = new System.Drawing.Point(161, 327);
+            this.IP_tbx.Multiline = true;
+            this.IP_tbx.Name = "IP_tbx";
+            this.IP_tbx.Size = new System.Drawing.Size(180, 39);
+            this.IP_tbx.TabIndex = 1;
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(347, 327);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(90, 39);
+            this.btn_connect.TabIndex = 2;
+            this.btn_connect.Text = "Anslut";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP Adress";
+            // 
+            // Message_tbx
+            // 
+            this.Message_tbx.Location = new System.Drawing.Point(12, 457);
+            this.Message_tbx.Multiline = true;
+            this.Message_tbx.Name = "Message_tbx";
+            this.Message_tbx.Size = new System.Drawing.Size(386, 61);
+            this.Message_tbx.TabIndex = 4;
+            this.Message_tbx.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(404, 457);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(104, 61);
+            this.btn_send.TabIndex = 5;
+            this.btn_send.Text = "Skicka";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(587, 98);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(426, 62);
+            this.logTextBox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(609, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Protokol";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(161, 372);
+            this.usernameTextBox.Multiline = true;
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(180, 39);
+            this.usernameTextBox.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Användarnamn";
+            // 
+            // btn_setUsername
+            // 
+            this.btn_setUsername.Location = new System.Drawing.Point(347, 375);
+            this.btn_setUsername.Name = "btn_setUsername";
+            this.btn_setUsername.Size = new System.Drawing.Size(90, 39);
+            this.btn_setUsername.TabIndex = 10;
+            this.btn_setUsername.Text = "Skicka";
+            this.btn_setUsername.UseVisualStyleBackColor = true;
+            this.btn_setUsername.Click += new System.EventHandler(this.btn_setUsername_Click);
+            // 
+            // profilePictureBox
+            // 
+            this.profilePictureBox.BackColor = System.Drawing.SystemColors.GrayText;
+            this.profilePictureBox.Location = new System.Drawing.Point(161, 12);
+            this.profilePictureBox.Name = "profilePictureBox";
+            this.profilePictureBox.Size = new System.Drawing.Size(180, 201);
+            this.profilePictureBox.TabIndex = 11;
+            this.profilePictureBox.TabStop = false;
+            // 
+            // btn_uploadPicture
+            // 
+            this.btn_uploadPicture.Location = new System.Drawing.Point(170, 219);
+            this.btn_uploadPicture.Name = "btn_uploadPicture";
+            this.btn_uploadPicture.Size = new System.Drawing.Size(97, 31);
+            this.btn_uploadPicture.TabIndex = 12;
+            this.btn_uploadPicture.Text = "Ladda Upp";
+            this.btn_uploadPicture.UseVisualStyleBackColor = true;
+            this.btn_uploadPicture.Click += new System.EventHandler(this.btn_uploadPicture_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 601);
+            this.Controls.Add(this.btn_uploadPicture);
+            this.Controls.Add(this.profilePictureBox);
+            this.Controls.Add(this.btn_setUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.Message_tbx);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_connect);
+            this.Controls.Add(this.IP_tbx);
+            this.Controls.Add(this.screen_tbx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.profilePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox screen_tbx;
+        private System.Windows.Forms.TextBox IP_tbx;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Message_tbx;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_setUsername;
+        private System.Windows.Forms.PictureBox profilePictureBox;
+        private System.Windows.Forms.Button btn_uploadPicture;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Client 1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Client_1
+{
+    public partial class Form1 : Form
+    {
+
+        private string username;// Variabel för att lagra användarnamnet
+
+        public Form1()
+        {
+            InitializeComponent();
+            ConfigureLogging();// Konfigurerar loggningen när programmet initieras
+        }
+        
+        UdpClient server;// UdpClient för att kommunicera med servern
+        IPEndPoint remoteIP;// Endpoint för den fjärranslutna servern
+        int remotPort = 55555; // Portnummer för fjärranslutningen
+        int port = 44444;  // Lokalt portnummer
+
+        // Metod för att konfigurera loggnings metod
+        private void ConfigureLogging()
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener("application.log"));// Loggning skickas till en fil
+            Trace.Listeners.Add(new TextBoxTraceListener(logTextBox)); // Loggning till logTextBox
+            Trace.AutoFlush = true; 
+        }
+
+        // Eventhanterare för när användaren klickar på "Connect"-knappen
+        
+        // Callback-metod som anropas när data tas emot
+        private void onReceive(IAsyncResult AR)
+        {
+            Log("Tar emot data...");
+            byte[] buff = server.EndReceive(AR, ref remoteIP);// Slutför mottagandet av data samt tar emot data
+            server.BeginReceive(new AsyncCallback(onReceive), server);// Börjar ta emot data igen 
+            string receivedMessage = Encoding.ASCII.GetString(buff);// Konverterar mottagen byte-array(data) till string variabel
+            Log($"Mottaget meddelande: {receivedMessage}");
+            ControlInvoke(screen_tbx, () => screen_tbx.AppendText(">>" + Encoding.ASCII.GetString(buff) + Environment.NewLine));// Visar meddelandet i screen_tbx
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Eventhanterare för när användaren klickar på "Send"-knappen
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            string messageToSend = Message_tbx.Text;
+            Log($"Skickar meddelande: {messageToSend}");
+            server.Connect(remoteIP);// Ansluter till den fjärranslutna IP och porten
+            server.Send(Encoding.ASCII.GetBytes(Message_tbx.Text), Message_tbx.Text.Length);// Skickar meddelandet som byte-array
+            Message_tbx.Clear();// Rensar textBox efter att meddelandet skickats
+            Log("Meddelande skickat.");
+
+        }
+
+        // Delegat för universell void-funktion
+        delegate void UniversalVoidDelegate();
+
+        // Metod för att utföra en åtgärd på en kontroll på ett säkert sätt
+        public static void ControlInvoke(Control control, Action function)
+        {
+            if (control.IsDisposed || control.Disposing)
+                return;
+
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new UniversalVoidDelegate(() => ControlInvoke(control, function)));
+                    return;
+            }
+            function();// Utför funktionen
+        }
+
+        // Metod för att logga meddelanden
+        private void Log(string message)
+        {
+            string logMessage = string.IsNullOrEmpty(username) ? message : $"{username}: {message}"; // Lägger till användarnamn om det finns
+            Trace.WriteLine($"{DateTime.Now}: {message}"); // Skriver loggmeddelandet med tidsstämpel
+        }
+
+        // Eventhanterare för när användaren klickar på "Set Username"-knappen
+        private void btn_setUsername_Click(object sender, EventArgs e)
+        {
+            username = usernameTextBox.Text.Trim(); // Hämtar och trimmar användarnamnet
+            if (!string.IsNullOrEmpty(username))
+            {
+                Log($"Användare: {username}"+"har loggat in"); // Loggar användarnamnet
+            }
+            else
+            {
+                Log("Användarnamn kan inte vara tomt. Snälla skriv in ett giltigt användarnamn."); // Loggar felmeddelande om användarnamnet är tomt
+            }
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            Log("Försökar att ansluta...");
+            server = new UdpClient(port); // Initialiserar UdpClient på den lokala porten
+            remoteIP = new IPEndPoint(IPAddress.Parse(IP_tbx.Text), remotPort);// Sätter fjärranslutningens IP och port
+            server.BeginReceive(new AsyncCallback(onReceive), server); // Börjar ta emot data 
+            Log($"Ansluten till {remoteIP.Address}:{remoteIP.Port}");
+
+        }
+
+        private void btn_uploadPicture_Click(object sender, EventArgs e)
+        {
+            // Konfigurerar OpenFileDialog för att välja bildfiler
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            openFileDialog1.Title = "Select a Profile Picture";
+
+            // Visar OpenFileDialog och kontrollerar om användaren valt en fil
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Laddar den valda bilden i PictureBox
+                profilePictureBox.Image = Image.FromFile(openFileDialog1.FileName);
+                Log("Profile picture uploaded."); // Loggar att profilbilden har laddats upp
+            }
+        }
+    }
+
+    // Anpassad TraceListener för att skriva loggar till en TextBox
+    public class TextBoxTraceListener : TraceListener
+    {
+        private readonly TextBox _target; // TextBox där loggar anteckans
+        private readonly StringSendDelegate _invokeWrite; // Delegat för att skicka strängar
+
+        public TextBoxTraceListener(TextBox target)
+        {
+            _target = target;
+            _invokeWrite = new StringSendDelegate(SendString);
+        }
+
+        public override void Write(string message)
+        {
+            _target.Invoke(_invokeWrite, new object[] { message }); // Skriv meddelandet till TextBox
+        }
+
+        public override void WriteLine(string message)
+        {
+            _target.Invoke(_invokeWrite, new object[] { message + Environment.NewLine });
+        }
+
+        private delegate void StringSendDelegate(string message); // Delegat för att skicka strängar
+
+        private void SendString(string message)
+        {
+            _target.AppendText(message); // Lägger till meddelandet i TextBox
+        }
+    }
+}
